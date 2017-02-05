4f6c348ace2751756821cb31174c3b8b64a92ce3
MaisAbuJarur <maisabujarur@gmail.com>
2017-02-05 11:41:52 +0200

    server up for text

--- server.js
+++ server.js
@@ -1,18 +1,49 @@
 var http = require('http');
-
+var express = require('express');
+var fs = require('fs');
+// var bodyParser = require("body-parser");
 //Lets define a port we want to listen to
 const PORT=8080; 
 
+// app.use(bodyParser.json());
+var app = express();
 //We need a function which handles requests and send response
-function handleRequest(request, response){
-    response.end('It Works!! Path Hit: ' + request.url);
-}
+// function handleRequest(request, response){
+//     response.end('It Works!! Path Hit: ' + request.url);
+// }
+// app.use(bodyParser.json()); // for parsing application/json
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/angular', function(req, res,callback) {
+	res.json('angular');
+	
+});
 
 
-var server = http.createServer(handleRequest);
+app.post('/angular', function(req, res) {
+	var body = "";
+	filePath = _dirname + 'archive.txt';
+	request.on('data', function(data){
+		body += data;
+	});
+	request.on('end', function (){
+		fs.appendFile(filePath, body, function(){
+			respond.end()
+		});
+	});
+
+});
+ // app.post('/login', function(req, res) {
+ //     console.log('Processing request...');
+ //     res.sendFile('/success.html');
+ // });
+
+
+var server = http.createServer(app);
 
 
 server.listen(PORT, function(){
     
     console.log("Server listening on: http://localhost:%s", PORT);
 });
+
+module.exports = app;
--- v2/app.js
+++ v2/app.js
@@ -1,13 +1,19 @@
 angular.module('images',[])
 
 
-.controller('imageController', function($scope){
+.controller('imageController', function($scope, $http){
 	$scope.images =[];
 	$scope.addImage = function(){
-		$scope.images.push($scope.newImage);
-		$scope.newImage = '';
-	}
+		$http.post('/angular',$scope.newImage).
+        success(function(data) {
+            console.log("posted successfully");
+        }).error(function(data) {
+            console.error("error in posting");
+        })
 	$scope.removeImage = function(index){
 		$scope.images.splice(index,1)
 	}
-});
+
+	
+};
+
