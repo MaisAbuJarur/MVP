10851409b69b7610d3124a7a7c055c2b4f13b9f7
MaisAbuJarur <maisabujarur@gmail.com>
2017-02-06 12:34:01 +0200

    v3

--- v2/app.js
+++ v2/app.js
@@ -6,6 +6,7 @@ angular.module('images',['ngRoute'])
 	$scope.addImage = function(){
 		$scope.images.push($scope.newImage);
 		$scope.newImage = '';
+		
 	}
 	$scope.removeImage = function(index){
 		$scope.images.splice(index,1)
